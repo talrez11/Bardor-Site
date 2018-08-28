fb49011cc4a612f27ef80ffcc2d1f89bf0a86b1b
talrez <tal@angelsense.com>
2018-08-28 21:54:11 +0300

    Update to social widget

--- wp-content/themes/bardor/includes/social.php
+++ wp-content/themes/bardor/includes/social.php
@@ -5,13 +5,18 @@
 	</a>
 	<ul>
 		<li>
-			<a href="mailto:talreznic11@gmail.com">
-				<img src="<?php echo get_stylesheet_directory_uri().'/images/facebook.png'; ?>" alt="facebook"/>
+			<a href="http://www.walla.co.il" target="_blank">
+				<img src="<?php echo get_stylesheet_directory_uri().'/images/linkedin.png'; ?>" alt="linkedin"/>
 			</a>
 		</li>
 		<li>
 			<a href="http://www.walla.co.il" target="_blank">
 				<img src="<?php echo get_stylesheet_directory_uri().'/images/mail.png'; ?>" alt="mail"/>
+			</a>
+		</li>
+		<li>
+			<a href="mailto:talreznic11@gmail.com">
+				<img src="<?php echo get_stylesheet_directory_uri().'/images/facebook.png'; ?>" alt="facebook"/>
 			</a>
 		</li>
 	</ul>
