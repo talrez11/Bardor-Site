053b2ea6257baf6d5abd87e8dac7c889ccb073be
talrez <tal@angelsense.com>
2020-04-21 21:22:46 +0300

    Added addthis share

--- wp-content/themes/bardor/footer.php
+++ wp-content/themes/bardor/footer.php
@@ -27,5 +27,8 @@
 
 <?php wp_footer(); ?>
 
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e9f388e0befe3ed"></script>
+
 </body>
 </html>
